b72e733ed49d321e62ce5e64c1122c4db2c3eaee
hmmmquan <95281540+hmmmquan@users.noreply.github.com>
2025-07-12 19:21:43 -0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect }   from 'react';
 import { useParams, Link }       from 'react-router-dom';
 import { supabase }              from '../supabaseClient';
-import defaultAvatar             from '../assets/default-avatar.jpg';
+import defaultAvatar             from '../assets/default avatar.jpg';
 
 export default function Profile() {
   const { username } = useParams();
